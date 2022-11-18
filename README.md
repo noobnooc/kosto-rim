95be9f37860079fd3775173bc0605da58057f4e2
Nooc <nooc@nooc.ink>
2022-11-18 11:11:33 +0800

    Initialize project structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # kosto-rim
+
+> ℹ️ The version of TypeScript is fixed on 4.8.8 because the newest version would cause module resolving issues in vscode.
+
+Every thing about Kosto
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import { App } from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
